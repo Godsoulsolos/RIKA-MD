345c481f8fb4ef255c014a0422d71b13d83638b0
Godsoulsolos <143340497+Godsoulsolos@users.noreply.github.com>
2023-11-03 19:24:38 +0100

    Update config.js
    Signed-off-by: Godsoulsolos <143340497+Godsoulsolos@users.noreply.github.com>

--- config.js
+++ config.js
@@ -7,20 +7,20 @@ global.owner = process.env.OWNER_NUMBER.split(",")
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
 global.email = 'sam@secktor.live'
-global.github = 'https://github.com/Layefa7/RIKA-MD'
+global.github = 'https://github.com/Godsoulsolos/KIRA-MD'
 global.location = 'Sultanpur IN'
 global.gurl = 'https://instagram.com/' // add your username
 global.sudo = process.env.SUDO || '2349070616688'
 global.devs = '2349070616688'
-global.website = 'https://github.com/Godsoulsolos/KIRA-MD' //wa.me/+994408223019
+global.website = 'https://github.com/Godsoulsolos/KIRA-MD' //wa.me/+2349070616688
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://camo.githubusercontent.com/189766685580e0c9506f5e5b5b4e7f3063951ea18f665925a10cb3c63e2bd2e5/68747470733a2f2f656e637279707465642d74626e302e677374617469632e636f6d2f696d616765733f713d74626e3a414e6439476351425074693464744c6c6d396c345355414932705a5551704276355f5a5a415257346c6c455a734d4a476c7a5268636e4c704776385065745126733d31302f6f75742e706e67'
 module.exports = {
   botname:   process.env.BOT_NAME === undefined ? 'SOUL-MD' : process.env.BOT_NAME,
   ownername: process.env.OWNER_NAME === undefined ? 'rika' : process.env.OWNER_NAME,
   sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID,
-  author:  process.env.PACK_INFO.split(";")[0] === undefined ? 'rika' : process.env.PACK_INFO.split(";")[0],
+  author:  process.env.PACK_INFO.split(";")[0] === undefined ? 'kira' : process.env.PACK_INFO.split(";")[0],
   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
-  packname:  process.env.PACK_INFO.split(";")[1] === undefined ? 'RIKA-MD' : process.env.PACK_INFO.split(";")[1],
+  packname:  process.env.PACK_INFO.split(";")[1] === undefined ? 'KIRA-MD' : process.env.PACK_INFO.split(";")[1],
   autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
   antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD,
   alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? 'false' : process.env.ALWAYS_ONLINE,
@@ -44,7 +44,7 @@ module.exports = {
     APP_NAME: process.env.HEROKU_APP_NAME === undefined ? '' : process.env.HEROKU_APP_NAME
 },
   VERSION: process.env.VERSION === undefined ? 'v.0.0.3' : process.env.VERSION,
-  LANG: process.env.THEME|| 'RIKA',
+  LANG: process.env.THEME|| 'LUFFY',
   WORKTYPE: process.env.WORKTYPE === undefined ? 'private' : process.env.WORKTYPE
 };
 
